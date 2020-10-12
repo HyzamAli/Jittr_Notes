392097317d2ff2d7914f258c45fbc5da67b8b0f2
HyzamAli <hyzamzz@gmail.com>
2020-10-12 21:53:54 +0530

    - Modified colors.xml - Added color shuffling capability to new Note

--- app/src/main/java/com/upsoul/jittrnotes/view/fragments/NewNoteFragment.java
+++ app/src/main/java/com/upsoul/jittrnotes/view/fragments/NewNoteFragment.java
@@ -1,20 +1,26 @@
 package com.upsoul.jittrnotes.view.fragments;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 
+import com.upsoul.jittrnotes.R;
 import com.upsoul.jittrnotes.data.models.Note;
 import com.upsoul.jittrnotes.databinding.FragmentNewNoteBinding;
+
+import java.util.Random;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 public class NewNoteFragment extends Fragment {
     FragmentNewNoteBinding binding;
-    private Note note;
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -25,10 +31,38 @@ public class NewNoteFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        //TODO: set random color and pass it as argument;
-        note = new Note(1);
+        Note note = new Note(generateColorIndex());
         binding.setNote(note);
+
+        binding.toolBar.setNavigationOnClickListener(view1 -> NavHostFragment.findNavController(this).popBackStack());
+        binding.btnAddNote.setOnClickListener(view1 -> hideKeyboard(requireActivity()));
     }
+
+    private int generateColorIndex() {
+        Random random = new Random();
+        int colorIndex = random.nextInt(10);
+        int[] colorsList = getResources().getIntArray(R.array.colors_list);
+        setColors(colorsList[colorIndex]);
+        return colorIndex;
+    }
+
+    public static void hideKeyboard(Activity activity) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        View view = activity.getCurrentFocus();
+        if (view == null) {
+            view = new View(activity);
+        }
+        //noinspection ConstantConditions
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    private void setColors(int color) {
+        binding.getRoot().setBackgroundColor(color);
+        binding.btnAddNote.setColorFilter(color);
+        binding.toolBar.getNavigationIcon().setTint(color);
+    }
+
 
     @Override
     public void onDestroyView() {
